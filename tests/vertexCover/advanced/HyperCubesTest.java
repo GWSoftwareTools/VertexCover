95b627d97161ad742c42dd4af47a2521010be097
Moritz <mgross321@gmail.com>
2019-09-16 01:02:33 +0200

    expanded test for hypercubes

--- tests/vertexCover/advanced/HyperCubesTest.java
+++ tests/vertexCover/advanced/HyperCubesTest.java
@@ -30,5 +30,11 @@ class HyperCubesTest {
         //added a cross-connection that is NOT allowed for a rectangle
         g.addEdge(1,4);
         assertEquals(0, HyperCubes.getRects(g).size());
+
+        g.deleteEdge(1,4);
+        g.addEdge(1,5);
+        g.addEdge(2,6);
+        g.addEdge(5,6);
+        assertEquals(2, HyperCubes.getRects(g).size());
     }
 }
